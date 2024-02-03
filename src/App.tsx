de495109f0c6ff76009226fb036ca9006b8daa3f
Sushant-Luitel <sushantluitel39@gmail.com>
2024-02-03 21:03:52 +0545

    set up typescript and implemented typesafety in props

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Person } from "./Person";
+
+function App() {
+  return (
+    <>
+      <Person name="sushant" age={22} email="sushantluitel" />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Person.tsx
@@ -0,0 +1,15 @@
+export interface Props {
+  name: string;
+  age: number;
+  email?: string;
+}
+
+export function Person({ name, age, email }: Props) {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{age}</h2>
+      <h1>Email:{email}</h1>
+    </div>
+  );
+}
